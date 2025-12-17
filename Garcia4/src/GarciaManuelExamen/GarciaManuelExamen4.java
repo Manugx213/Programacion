5cd28984c51223a72073a01d7aaa796d554b61a7
manugx213 <manugx213@gmail.com>
2025-12-17 18:41:39 +0100

    ole

---
+++ Garcia4/src/GarciaManuelExamen/GarciaManuelExamen4.java
@@ -0,0 +1,15 @@
+/* MANUEL GARCIA GARCIA
+ * EXAMEN 4 
+ * 17-12-2025
+ */
+
+package GarciaManuelExamen;
+
+public class GarciaManuelExamen4 {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
